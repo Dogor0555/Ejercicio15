b5738a1bf4ec6ff5274e8ba447f995e75e957f01
Dogor0555 <74686946+Dogor0555@users.noreply.github.com>
2025-02-15 06:23:26 -0600

    Ejercicio15

--- Ejercicio15/Program.cs
+++ Ejercicio15/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+int numero;
+
+do
+{
+    Console.Write("Ingrese un número positivo: ");
+    numero = int.Parse(Console.ReadLine());
+}
+while (numero <= 0);
+
+Console.WriteLine("¡Número válido ingresado! El número es: " + numero);
